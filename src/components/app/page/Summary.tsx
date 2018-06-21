db5d1abbe95557ccf135b364ccd5c49cde36299d
Maxim Kasatkin <mvkasatkin@gmail.com>
2018-06-22 01:01:23 +0700

    dev

--- src/components/app/page/Summary.tsx
+++ src/components/app/page/Summary.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
+import Heading1 from 'components/shared/ui/Heading1'
 
 interface Props {
 }
 
-const PageSummary: React.StatelessComponent<Props> = props => (<div>summary</div >)
+const PageSummary: React.StatelessComponent<Props> = props => (
+  <div>
+    <Heading1>Выписка</Heading1>
+  </div>
+)
 
 export default PageSummary
